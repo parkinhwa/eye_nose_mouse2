e03d334d87be78a340f3b2ae2acef8af135a63ac
박인화B <ihwapark@smilegate.sg>
2026-05-21 20:31:27 +0900

    fix: electron 빌드 추가

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile('index.html');
+
+  // 개발 중에는 DevTools 열기 (배포 시 주석 처리 가능)
+  // win.webContents.openDevTools();
+}
+
+if (app) {
+  app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+      }
+    });
+  });
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+}
